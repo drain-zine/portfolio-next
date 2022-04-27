046178fd031e69af842ea8f9db115342f96577fd
Tom Stannett <invisibletree@protonmail.ch>
2022-04-27 10:34:11 +0100

    index v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,22 +21,13 @@ const Home = () => {
             <motion.div 
                 className={styles.animatableContainer}
                 variants={staggerVariants}
-                initial={'initial'}
-                animate={'in'}
-                exit={'out'}
             >
                 {/* <div className={styles.line}/> */}
                 <motion.section 
                 className={styles.nav}
-                            variants={staggerVariants}
-            initial={'initial'}
-            animate={'in'}
-            exit={'out'}>
+                            variants={staggerVariants}>
                     <motion.h3
                         variants={variants}
-                        initial={'initial'}
-                        animate={'in'}
-                        exit={'out'}
                     >
                         contents</motion.h3>
                     <motion.div className={styles.links}>
@@ -45,24 +36,15 @@ const Home = () => {
                         >
                             <motion.h3
                                 variants={variants}
-                                initial={'initial'}
-                                animate={'in'}
-                                exit={'out'}
                             >
                                 design</motion.h3>
                         </Link>
                         <motion.h3                        
                             variants={variants}
-                            initial={'initial'}
-                            animate={'in'}
-                            exit={'out'}
                         >
-                            other</motion.h3>
+                            digital posters</motion.h3>
                         <motion.h3                        
                             variants={variants}
-                            initial={'initial'}
-                            animate={'in'}
-                            exit={'out'}
                         >
                             details</motion.h3>
                     </motion.div>
@@ -73,13 +55,11 @@ const Home = () => {
                             <motion.line
                                 variants={{
                                     in: {x: '0%'},
-                                    out: {x: '-100%'}
+                                    out: {x: '-100%'},
+                                    initial: {x: '0%'}
 
                                 }} 
-                                    initial={'in'}
-                                    animate={'in'}
-                                    exit={'out'}
-                                    transition={{duration: 0.8 }}
+                                    // transition={{duration: 0.8 }}
                                 x1="0" 
                                 x2="100%" 
                                 y1="50%" 
@@ -93,20 +73,20 @@ const Home = () => {
                         <rect width="100%" height="100%" fill="white"/>
                         <use href='#lines' stroke='black'/>
                     </mask>
-                    <use href='#circle' fill='black' stroke='none'/>
-                    <use href="#circle" fill="white" stroke='none' mask="url(#mask)"/>
+                    <use href='#circle' fill='black' /*stroke='none'*//>
+                    <use href="#circle" fill="white" /* stroke='none' */ mask="url(#mask)"/>
                 </svg>
             </motion.div>
              <motion.section 
                 variants={variants}
-                initial={'initial'}
-                animate={'in'}
-                exit={'out'}
                 className={styles.content}
                 >
-                <div className={styles.title}>
+                {/* <div className={styles.title}>
                     <h1>Tom</h1>
                     <h1>Stannett</h1>
+                </div> */}
+                <div className={styles.title2}>
+                    <p>Tom Stannett</p>
                 </div>
                 {/*<div className={styles.nav}>
                     <Link
@@ -121,7 +101,6 @@ const Home = () => {
             <motion.section
                         className={styles.blackBackground}
                         variants={slideVariants}
-                        out={'out'}
                     >
             </motion.section>
         </motion.main>
